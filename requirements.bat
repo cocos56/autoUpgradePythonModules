b2fae2609ca591bc82ab9065f3f3a7ac657dad10
7 <zj175@139.com>
2020-08-06 23:50:46 +0800

    更新于2020年08月06日	23:50:46.55

--- requirements.bat
+++ requirements.bat
@@ -1,8 +1,5 @@
-@echo off
 %1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
 cd /d "%~dp0"
-
-python -m pip install --upgrade pip
 
 pip install -i https://pypi.douban.com/simple/ -r requirements.txt
 
