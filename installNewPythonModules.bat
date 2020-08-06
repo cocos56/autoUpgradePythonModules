be68845712b4b6eb0889344eb8622326e0670f5c
7 <zj175@139.com>
2020-08-06 23:39:41 +0800

    更新于2020年08月06日	23:39:40.96

--- installNewPythonModules.bat
+++ installNewPythonModules.bat
@@ -1,9 +1,7 @@
-@echo off
 %1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
 cd /d "%~dp0"
 
 set /p name=请输入要安装的库名：
-echo 正在安装%name%
 
 pip install %name% -i https://pypi.douban.com/simple/
 
